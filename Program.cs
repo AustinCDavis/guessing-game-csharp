3dc0ad0def1651743b7e61fefda9d151324fe335
Austin Davis <davis.austin1.1.c@gmail.com>
2023-06-11 10:20:44 -0400

    Phase 1 complete

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Phase 1
+
+//  Display message asking user to guess secret number
+// display a prompt for the user's guess
+//take the user's guess as input and save it as a variable
+// Display the user's guess back tp the screen
+
+string userGuess;
+int userInt;
+
+try
+{
+    Console.Write("Please guess the secret number: ")
+
+    userGuess = Console.ReadLine();
+
+    userInt = int.Parse(userGuess);
+
+    Console.WriteLine($"You guessed {userInt}!")
+}
+catch
+{
+    Console.WriteLine("Hey loser, you need to input an integer!")
+}
